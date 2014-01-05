bc71d065cbbb832ac4baed3847d39c8415bccb73
robertoarruda <robertoadearruda@gmail.com>
2014-01-05 14:33:24 -0200

    Correcao do Config do cake pos atualizacao

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93bndjhdjfIxfs2guVoUubWwvniR2G0Fgkjj9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7685930965745354249674969767865');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
